0dfbf991c8a8aea062d734f78cdc92a8569c40bf
Anna-Maria13 <fostera2017@fau.edu>
2024-04-17 12:11:36 -0400

    dataprocessor

--- src/Dataprocessor.java
+++ src/Dataprocessor.java
@@ -124,15 +124,38 @@ public class DataProcessor {
         }
     }
 
-    // Define Cell class
-    static class Cell {
-        private String oem;
-        private String model;
-        private String launchAnnounced;
-        private String launchStatus;
-        private String bodyDimensions;
-        private String bodyWeight;
-        private String bodySim;
-        private String displayType;
+   // Define Cell class
+   static class Cell {
+    private String oem;
+    private String model;
+    private String launchAnnounced;
+    private String launchStatus;
+    private String bodyDimensions;
+    private String bodyWeight;
+    private String bodySim;
+    private String displayType;
+    private String displaySize;
+    private String displayResolution;
+    private String featuresSensors;
+    private String platformOs;
 
+    // Constructor
+    public Cell(String oem, String model, String launchAnnounced, String launchStatus,
+                String bodyDimensions, String bodyWeight, String bodySim, String displayType,
+                String displaySize, String displayResolution, String featuresSensors, String platformOs) {
+        this.oem = oem;
+        this.model = model;
+        this.launchAnnounced = launchAnnounced;
+        this.launchStatus = launchStatus;
+        this.bodyDimensions = bodyDimensions;
+        this.bodyWeight = bodyWeight;
+        this.bodySim = bodySim;
+        this.displayType = displayType;
+        this.displaySize = displaySize;
+        this.displayResolution = displayResolution;
+        this.featuresSensors = featuresSensors;
+        this.platformOs = platformOs;
+    }
+
+    //Getters and
 
